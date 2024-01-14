1d9767ba05adb02a4e1d3a21d7967b3b3547226d
Artem <levkin_mizuno@mail.ru>
2024-01-14 16:41:04 +0700

    hw

---
+++ catalog/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+
+from catalog.models import Product, Version
+
+
+class StyleFormMixin:
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ('name', 'desc',)
+
+    def clean_name(self):
+        cleaned_data = self.cleaned_data['name']
+        for word in ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']:
+            if word in cleaned_data:
+                raise forms.ValidationError('Присутствуют недопустимые одно или несколько слов!')
+        return cleaned_data
+
+    def clean_desc(self):
+        cleaned_data = self.cleaned_data['desc']
+        for word in ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']:
+            if word in cleaned_data:
+                raise forms.ValidationError('Присутствуют недопустимые одно или несколько слов!')
+        return cleaned_data
+
+
+class VersionForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Version
+        fields = '__all__'
+
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import index_1, ProductListView
+from catalog.views import index_1, ProductListView, ProductCreateView, ProductUpdateView
 
 app_name = CatalogConfig.name
 urlpatterns = [
     path('', ProductListView.as_view(), name='index'),
-    path('contacts/', index_1, name='index_1'),
+    path('contact/', index_1, name='index_1'),
+    path('create/', ProductCreateView.as_view(), name='create_product'),
+    path('update/<int:pk>/', ProductUpdateView.as_view(), name='update_product'),
 ]
